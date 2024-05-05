c33f2b5d55ea85df5652e77593e6fb0a01bfde12
caeklol <me@caek.dev>
2024-05-05 16:31:14 +0800

    use nh (nix helper)

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -9,13 +9,20 @@
       auto-optimise-store = true;
     };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 1d";
-    };
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 1d";
+    # };
 
     registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
     nixPath = ["nixpkgs=${inputs.nixpkgs.outPath}"];
   };
+
+	programs.nh = {
+    	enable = true;
+    	clean.enable = true;
+    	clean.extraArgs = "--keep-since 1d --keep 3";
+    	flake = "/etc/nixos";
+  	};
 }
