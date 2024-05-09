fd590babdf38b7b547e358bb8f44c27ba3dd27fe
caeklol <me@caek.dev>
2024-05-09 19:22:58 +0800

    remove remnants of old xmrig-switch and update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,4 @@
 # You can build them using 'nix build .#example'
 {pkgs ? (import ../nixpkgs.nix) {}}: {
   apple-fonts = pkgs.callPackage ./apple-fonts.nix {};
-  xmrig-switch = pkgs.callPackage ./xmrig-switch.nix {};
 }
