ef0cfd86402537c8dce2251ca59e9f4c43987e38
caeklol <me@caek.dev>
2024-06-25 13:13:18 +0800

    mullvad fixes

--- hosts/common/mullvad.nix
+++ hosts/common/mullvad.nix
@@ -1,4 +1,8 @@
 { ... }:
 {
+	services.resolved.enable = true;
+  	networking.firewall.checkReversePath = "loose";
+	networking.iproute2.enable = true;
+	networking.wireguard.enable = true;
 	services.mullvad-vpn.enable = true;
 }
